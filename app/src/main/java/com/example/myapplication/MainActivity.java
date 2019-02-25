13778740648c59d9dacbb56c31ef5052a1d38857
shafinr23 <shafinr23@gmail.com>
2019-02-25 21:14:27 +0600

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+
+    @Override
+    public void onBackPressed(){
+        if (webview.canGoBack()){
+            webview.goBack();
+        }else{
+
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        webview = (WebView)findViewById(R.id.webview);
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://college-updates.shafinrahman.org/");
+    }
+}
